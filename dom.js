436396a4f2c50ec32ab8c9ac67548cf8cbcdd7a2
ito guti <memecito@gmail.com>
2024-05-14 10:15:11 +0200

    continuamos familiarizandonos con github

---
+++ dom.js
@@ -0,0 +1,10 @@
+//manejo del DOM
+<html lang="en">
+
+<body>
+    <h1 id="Saludo">Hola Mundo</h1>
+    
+</body>
+</html>
+const elementoSaludo= document.getElementById('salud');
+elementoSaludo.innerHTML="Adiós mundo cruel";
--- objetos.js
+++ objetos.js
@@ -1,16 +1,48 @@
 //objetos
 /*tipo de dato para almacenar múltiples coleeciones de datos mediante clave/valor*/
-try{
-    const persona = {
-        nombre: 'Antonio',
-        apellido:'Guti',
-        direccion:'jose lopez arjona',
-        saludo: function() {console.log('Soy Antonio Gutierre')}
-    };
+const persona = {
+    nombre: 'Antonio',
+    apellido:'Guti',
+    direccion:'jose lopez arjona',
+    saludo: function() {console.log('Soy Antonio Gutierre')}
+};
+const numeros= {a:1,b:2,c:3,d:4};
+const listadoUsuarios=[
+{nombre: "AAA",edad:1,signo:"Geminis"},
+{nombre: "bbb",edad:2,signo:"Tauro"},
+{nombre: "ccc",edad:3,signo:"Piscis"},
+{nombre: "ddd",edad:4,signo:"Virgo"},
+{nombre: "eee",edad:5,signo:"Acuario"},
+
+];
+
+
+
+try{   
     console.log(persona['nombre']);
     console.log(persona['apellido']);
     console.log("hola mundo");
     persona.saludo();
+    //recorrer bucle con dos metodos
+    /*
+    for (const clave in numeros){
+        console.log(`${clave}: ${numeros[clave]}`);    }
+        */
+    Object.keys(numeros).forEach(function(clave){
+        console.log(`${clave}: ${numeros[clave]}`);
+    });
+
+
+    //obtener nombre
+    const soloNombre=[];
+    listadoUsuarios.forEach(function(usuario){soloNombre.push(usuario.nombre);});
+    //Manera mas sencilla
+    const soloNOmbre2= listadoUsuarios.map(function(usuario){return usuario.nombre;});
+    //Edad total, con funcion tipo flecha ("arrow")
+    // El segundo parámetro que recibe reduce es el valor inicial del
+    //acumulador en este caso, es 0
+    const edadTotal= listadoUsuarios.reduce((acc,usuario)=>acc + usuario.edad, 0);
+    console.log(`edad total: ${edadTotal}`);
 }catch(e){
     console.log("ALGO HA SALIDO MAL");
     console.log(e);
